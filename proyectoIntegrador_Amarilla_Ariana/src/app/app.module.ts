838b495435a530273fc48b7d42c0d879ddb658b4
MghAriana <arianaamarilla8@gmail.com>
2023-03-23 12:35:19 -0300

    Acerca de

--- proyectoIntegrador_Amarilla_Ariana/src/app/app.module.ts
+++ proyectoIntegrador_Amarilla_Ariana/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './componentes/header/header.component';
 import { LogoComponent } from './componentes/logo/logo.component';
 import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './componentes/banner/banner.component';
     HeaderComponent,
     LogoComponent,
     RedesSocialesComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
