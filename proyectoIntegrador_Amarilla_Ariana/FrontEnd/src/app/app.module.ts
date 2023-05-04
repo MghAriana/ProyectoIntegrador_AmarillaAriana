2ad9d9c9a262bc0ac0765320cc57254cb00f0894
MghAriana <arianaamarilla8@gmail.com>
2023-05-04 12:27:34 -0300

    creacion de carpeta frontend

--- proyectoIntegrador_Amarilla_Ariana/FrontEnd/src/app/app.module.ts
+++ proyectoIntegrador_Amarilla_Ariana/FrontEnd/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoComponent } from './componentes/logo/logo.component';
-import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './componentes/banner/banner.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
@@ -20,7 +18,6 @@ import { FooterComponent } from './componentes/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoComponent,
-    RedesSocialesComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
